9f048400e8fcb8768239743a961f6e6fc5251e5b
tomohide terabe <tomohidex@gmail.com>
2024-08-03 17:16:42 +0900

    topメニュー

---
+++ src/lib/GenreSelector.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import {genre} from '$lib/genre';
+</script>
+
+<div>
+	<ul>
+		{#each genre as {
+			name,label
+		}}
+			<li>
+				<a
+					href="/genre/{name}"
+				>{label}</a>
+			</li>
+		{/each}
+	</ul>
+</div>
